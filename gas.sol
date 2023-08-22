d0dcaa63aea74e372b7726132978163599644cff
Erik <esnetto@gmail.com>
2023-08-22 17:45:43 -0300

    refactor

--- gas.sol
+++ gas.sol
@@ -6,32 +6,15 @@ import "./helper.sol";
 // Run with medusa fuzz --target gas.sol --deployment-order fuzzIt
 
 contract fuzzIt is PropertiesAsserts {
-    error ETHTransferFailed();
-
-    function fuzzSafeTransferETH(address to, uint256 amount) external payable {
-        uint256 balanceBefore = to.balance;
-        emit LogUint256("Balance Before", balanceBefore);
-        safeTransferETH(to, amount);
-        safeTransferETH2(to, amount);
-        emit LogUint256("Balance After", to.balance);
-        assertEq(balanceBefore + amount * 2, to.balance, "Balance is not right");
-    }
-
-    function safeTransferETH(address to, uint256 amount) internal {
+    function fuzz_SafeTransferETH(address to) external payable {
+        uint256 amount = msg.value / 2;
+        bool success1;
+        bool success2;
         assembly {
-            if iszero(call(gas(), to, amount, gas(), 0x00, gas(), 0x00)) {
-                mstore(0x00, 0xb12d13eb)
-                revert(0x1c, 0x04)
-            }
+            success1 := call(gas(), to, amount, gas(), 0x00, gas(), 0x00)
+            success2 := call(gas(), to, amount, 0x00, 0x00, 0x00, 0x00)
         }
-    }
 
-    function safeTransferETH2(address to, uint256 amount) internal {
-        assembly {
-            if iszero(call(gas(), to, amount, 0x00, 0x00, 0x00, 0x00)) {
-                mstore(0x00, 0xb12d13eb)
-                revert(0x1c, 0x04)
-            }
-        }
+        assertEq(success1, success2, "SafeTransferETH failed");
     }
 }
